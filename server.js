3119288dc2d4f763039c0373d92d267d428a219e
Jeff <jcgilbert70@gmail.com>
2022-12-10 12:10:39 -0600

    added files and ode for wildcard route to send users to a 404 page, used miniproject code

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require("express");
 
-const app = express();
-
 const PORT = process.env.PORT || 5050;
 
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+// Middleware for parsing JSON and URLencoded form data
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+// Wildcard route to direct users to a 404 page
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/pages/404.html'))
+);
+
+// require to run app
 app.listen(PORT, () => {
   console.log(`App Port: ${PORT}`);
 });
